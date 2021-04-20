5002370d3153e91e92c02b0137e61392e10197d7
dhruv331 <dhruv.agarwal3131@gmail.com>
2021-04-20 13:19:50 +0530

    api-change

--- src/actions/appsList.js
+++ src/actions/appsList.js
@@ -2,7 +2,7 @@ import { SET_APPS_LIST } from "../actionTypes/appsActionType";
 
 
 export const getAppsList = () => async(dispatch) => {
-  let res=await fetch('https://api.npoint.io/4ca5aaf459a573940672')
+  let res=await fetch('https://api.npoint.io/adf6676a313fa01f787d')
   let parsed= await res.json()
   dispatch({ type: SET_APPS_LIST, payload:parsed });
 };
